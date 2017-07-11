d4af101ffe86007a3f22683c4611022a17b90316
Kevin Genus <kevin.genus@gmail.com>
2017-07-11 13:02:58 -0400

    Dynamic routing

--- client/components/NewChannelEntry.js
+++ client/components/NewChannelEntry.js
@@ -1,50 +1,51 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { addChannel, postChannel } from '../store'
+import {connect} from 'react-redux'
+import {addChannel, postChannel} from '../store'
 
-function mapStateToProps (state) {
-  return {
-    newChannelEntry: state.newChannelEntry
-  }
+function mapStateToProps(state) {
+    return {
+        newChannelEntry: state.newChannelEntry
+    };
 }
 
-function mapDispatchToProps (dispatch) {
-  return {
-    // handleChange: function () {
-    //   dispatch(addChannel(event.target.value))
-    // }
-    handleChange (event) {
-      dispatch(addChannel(event.target.value))
-    },
-    handleSubmit (event) {
-      event.preventDefault()
-      dispatch(postChannel({ name: event.target.channelName.value }))
-      dispatch(addChannel(''))
-    }
-  }
+function mapDispatchToProps(dispatch, ownProps) {
+    return {
+        // handleChange: function () {
+        //   dispatch(addChannel(event.target.value))
+        // }
+        handleChange (event) {
+            dispatch(addChannel(event.target.value));
+        },
+        handleSubmit (event) {
+            event.preventDefault();
+            dispatch(postChannel({name: event.target.channelName.value}));
+            dispatch(addChannel(''));
+            ownProps.history.push();
+        }
+    };
 }
 
-function NewChannelEntry (props) {
-  return (
-    <form onSubmit={ props.handleSubmit } >
-      <div className="form-group">
-        <label htmlFor="name">Create a Channel</label>
-        <input
-          onChange={ props.handleChange }
-          className="form-control"
-          type="text"
-          name="channelName"
-          placeholder="Enter channel name"
-          value={ props.newChannelEntry }
-        />
-      </div>
-      <div className="form-group">
-        <button type="submit" className="btn btn-default">Create Channel</button>
-      </div>
-    </form>
-  );
+function NewChannelEntry(props) {
+    return (
+        <form onSubmit={ props.handleSubmit }>
+            <div className="form-group">
+                <label htmlFor="name">Create a Channel</label>
+                <input
+                    onChange={ props.handleChange }
+                    className="form-control"
+                    type="text"
+                    name="channelName"
+                    placeholder="Enter channel name"
+                    value={ props.newChannelEntry }
+                />
+            </div>
+            <div className="form-group">
+                <button type="submit" className="btn btn-default">Create Channel</button>
+            </div>
+        </form>
+    );
 }
 
 /** Write your `connect` component below! **/
-const Container = connect(mapStateToProps, mapDispatchToProps)(NewChannelEntry)
-export default Container
+const Container = connect(mapStateToProps, mapDispatchToProps)(NewChannelEntry);
+export default Container;
